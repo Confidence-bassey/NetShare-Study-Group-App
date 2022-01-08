25fe819977f527168bdc3a0c0b1fe8f025d7b36e
simonbassey <simon.dev.bassey@gmail.com>
2022-01-08 19:54:52 +0100

    updated entity definitions and added mysql dependencies

---
+++ src/main/java/com/zealmobile/studygroup/core/entities/GroupMember.java
@@ -0,0 +1,40 @@
+package com.zealmobile.studygroup.core.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.zealmobile.studygroup.core.models.enums.GroupMembershipStatus;
+import com.zealmobile.studygroup.core.models.enums.GroupMembershipType;
+
+@Entity
+@Table(name="group_users")
+public class GroupMember {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name="group_id")
+    private int groupId;
+
+    @Column(name="membership_type")
+    private GroupMembershipType membershipType;
+
+    @Column(name="membership_status")
+    private GroupMembershipStatus membershipStatus;
+
+
+    @ManyToOne()
+    @JoinColumn(name="group_id", referencedColumnName="id", insertable=false, updatable=false)
+    private Group Group;
+    
+}
